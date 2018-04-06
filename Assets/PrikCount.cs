9fefe192a4de3edf768029b44fb52498e0ce2951
Abbe-Engers <abbeilias.engers@coderclass.nl>
2018-04-06 19:03:18 +0200

    add@ score

---
+++ Assets/PrikCount.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrikCount : MonoBehaviour
+{
+
+    Text PrikkerDisplay;
+
+    // Use this for initialization
+    void Start()
+    {
+        PrikkerDisplay = GetComponent<Text>();
+
+        Counter.prik = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {            
+        PrikkerDisplay.text = Counter.prik.ToString();
+    }
+}
+
+
+
+public static class Counter
+{
+    public static int prik;
+}
